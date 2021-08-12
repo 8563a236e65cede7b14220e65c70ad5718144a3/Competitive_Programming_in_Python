23cbd81047596eb0f9e3ec6db0fca2f848bd4717
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-12 12:15:20 +0200

    (Test) sphinx math typesetting
    Why: See if math extension works after dependency installation
    
    How: Install dependencies as per README.md and see if docs build
    
    Tags: documentation math

--- Chapter01/matrix_input_output.py
+++ Chapter01/matrix_input_output.py
@@ -4,5 +4,5 @@ Matrix Input Output
 
 This file contains some utility functions for reading from standard input. These cover single integers,
 integer arrays and matrices. Also implemented is *Freivalds test* (1979) to determine, probabilistically
-whether the equality
+whether the equality :math:`AB = C` holds for :math:`n*n` matrices :math:`A, B, C` .
 """
