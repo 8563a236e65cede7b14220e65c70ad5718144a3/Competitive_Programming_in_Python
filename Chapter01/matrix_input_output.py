4ec3abccd12808eb280346d20624760faee822c8
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-12 14:25:33 +0200

    (Add) convert_anystr (Update) readint (Document) readarray
    Why: AnyStr returned from readline() may be bytes or str. Ensure that value
         is str type when logging.
    
    How: Check if string is an instance of str or bytes and handle exceptions
    
    Tags: readarray

--- Chapter01/matrix_input_output.py
+++ Chapter01/matrix_input_output.py
@@ -9,7 +9,7 @@ whether the equality :math:`AB = C` holds for :math:`n*n` matrices :math:`A, B, 
 import os
 import sys
 import logging
-from typing import Type, Any
+from typing import Type, Any, AnyStr
 from random import randint
 from pathlib import Path
 
@@ -37,9 +37,22 @@ def flush_buffers_and_exit(error: int) -> None:
     sys.exit(error)
 
 
+def convert_anystr(any_str: AnyStr) -> str:
+    """
+    Helper function to take an :class:`.AnyStr` type and return :class:`str` output. Returns :class:`str` input
+    unmodified but decodes :class:`bytes` input to :class:`str`.
+
+    :param typing.AnyStr any_str: The :class:`str` or :class:`bytes` object to coerce.
+    :rtype: str
+    :return: :class:`str` value of any_str.
+    """
+    return any_str if isinstance(any_str, str) else any_str.decode()
+
+
 def readint() -> int:
     """
-    Reads an integer from :code:`stdin`. This function expects a single line of input with only an integer present.
+    Reads an integer from :code:`stdin`. This function expects a single line of input with only an integer present. If
+    the input value is not an integer, the program exits.
 
     :rtype: int
     :return: The integer held in the :code:`stdin` buffer.
@@ -47,25 +60,52 @@ def readint() -> int:
     # Initialize storage.
     value: int = 0
 
+    # Read the line first.
+    stdin_input_str: AnyStr = sys.stdin.readline()
+
     # The function is expecting a single integer input. We must handle the case where the input is a single integer.
     try:
         # The input was a recognizable integer.
-        value = int(sys.stdin.readline())
+        value = int(stdin_input_str)
     except ValueError as err:
         # The input was not a recognizable integer. Log the error and exit the program.
-        logger.critical("readint - " + str(err))
+        logger.critical(
+            "readint - " + str(err) +
+            "\nInput: " + convert_anystr(stdin_input_str)
+        )
         flush_buffers_and_exit(os.EX_DATAERR)
 
     return value
 
 
-def readarray(typ: Type):
+def readarray(typ: Type) -> list[Any]:
+    """
+    Reads in an array of a given type from :code:`stdin`. If the elements within :code:`stdin` are not all of the
+    correct type, the program exits.
+
+    :param typing.Type typ: The type of the elements of the list.
+    :rtype: list[Any]
+    :return: A list created from the :code:`stdin` input line.
+    """
+    # Initialize storage.
     array: list[typ]
+
+    # Read the line.
+    stdin_input_str: AnyStr = sys.stdin.readline()
+
+    # We attempt to map the input to a list of appropriate type.
     try:
-        array = list(map(typ, sys.stdin.readline().split()))
+        # All the entries in the input line were of the correct type.
+        array = list(map(typ, stdin_input_str.split()))
     except ValueError as err:
-        logger.critical("readarray - " + str(err))
+        # At least one of the entries in the input line was of an incorrect type. We log the error message and exit
+        # with an os.EX_DATAERR
+        logger.critical(
+            "readarray - " + str(err) +
+            "\nInput: " + convert_anystr(stdin_input_str)
+        )
         flush_buffers_and_exit(os.EX_DATAERR)
+
     return array
 
 
