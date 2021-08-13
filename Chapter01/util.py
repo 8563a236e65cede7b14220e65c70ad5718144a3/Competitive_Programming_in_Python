e00b5d2af868b7b0b4bf9f6317afc31ca4bc41ce
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-13 15:34:22 +0200

    (Add) majority function
    Why: Work out which word or element appears most frequently in
         a list
    
    How: Use a dictionary to count the number of occurrences.
    
    Tags: majority

--- Chapter01/util.py
+++ Chapter01/util.py
@@ -20,3 +20,33 @@ def max_index_value(tab: list[NumT]) -> tuple[NumT, int]:
     :return: A tuple representing the element and its index.
     """
     return max((tab[i], i) for i, tab_i in enumerate(tab))
+
+
+def majority(L: list[str]) -> str:
+    """
+    Find the most common element of an array. This implementation has average complexity :math:`O(nk)` and a worst case
+    complexity of :math:`O(n^{2}k)` . The algorithm yields the first word encountered with the highest number of
+    occurrences.
+
+    :param list[str] L: The words to count the frequency of, given as a list.
+    :return: The word with the highest number of occurrences.
+    """
+    # Initialize storage for counter and iterator.
+    count: dict[str, int] = {}
+    word: str
+
+    # Count the number of occurrences for each word in the list.
+    for word in L:
+        # If the word has already been encountered
+        if word in count:
+            # Increment the counter by 1.
+            count[word] += 1
+        # Otherwise this is the first time we are seeing the word
+        else:
+            # Initialize the counter to 1.
+            count[word] = 1
+
+    # Yield the first word with maximal count.
+    val_1st_max, arg_1st_max = min((-count[word], word) for word in count)
+
+    return arg_1st_max
