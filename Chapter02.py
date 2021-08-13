9285a25c28d2a067df47b7d96f076ac4e7915079
Warren Jitsing <warren.jitsing@gmail.com>
2021-08-13 18:23:04 +0200

    (Refactor) Chapter02 into module
    Why: Provide shorter namespaces in documentation.
    
    How: Delete Chapter02 directory and create Chapter02 file and autodoc
    
    Tags: refactor

---
+++ Chapter02.py
@@ -0,0 +1,6 @@
+"""
+Introduction
+------------
+
+This module groups the code for Chapter 2 of Competitive Python Programming.
+"""
--- docs/source/Chapter02.rst
+++ docs/source/Chapter02.rst
@@ -1,2 +1,7 @@
 Chapter02
 =========
+
+.. automodule:: Chapter02
+   :members:
+   :undoc-members:
+   :show-inheritance:
